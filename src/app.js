399d416fe77e0a633df08292510c5c0ca2b87ef9
Arfun Ali Yafie <alinews120@gmail.com>
2026-04-21 01:45:00 +0700

    add bio and footer

--- src/app.js
+++ src/app.js
@@ -3,6 +3,10 @@ function hamburger() {
     MobileMenu.classList.toggle("hidden");
 }
 
+// Current Year
+const currentYear = new Date().getFullYear();
+document.getElementById("currentYear").textContent = currentYear;
+
 // TypeIt
 document.addEventListener("DOMContentLoaded", function () {
     new TypeIt("#typeIT", {
@@ -13,5 +17,17 @@ document.addEventListener("DOMContentLoaded", function () {
         .pause(200)
         .delete(19, { speed: 150, startDelay: 500 })
         .type("My Name Arfun Ali Yafie", { delay: 150, startDelay: 500 })
+        .go();
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    new TypeIt(".about", {
+        speed: 30,
+        startDelay: 100,
+        strings: [""],
+    })
+        // .pause(200)
+        // .delete(19, { speed: 150, startDelay: 500 })
+        // .type("My Name Arfun Ali Yafie", { delay: 150, startDelay: 500 })
         .go();
 });
